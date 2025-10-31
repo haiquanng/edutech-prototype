3ebca6f0485d6eb66fe35be6711eb00102fddf65
haiquanng <nguyenhaiquan.data@gmail.com>
2025-10-31 10:56:42 +0700

    feat: add instruction

--- class/list/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.back').onclick = () => history.back();
-document.querySelector('.search').addEventListener('input', e=>{
-  const term = e.target.value.toLowerCase();
-  document.querySelectorAll('#tbody tr').forEach(tr=>{
-    tr.style.display = tr.textContent.toLowerCase().includes(term) ? '' : 'none';
-  });
-});
